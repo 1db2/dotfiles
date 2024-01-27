fc8a82e0ffe820b1070084614fa78404862638c9
idb <86549645+1db2@users.noreply.github.com>
2024-01-27 16:08:54 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-current_path=$(pwd)
-
 apps=("kitty" "polybar" "rofi" "polkit" "polkit-kde-agent" "feh" "flameshot" "meson" "ninja" "libconfig" "uthash" "python-i3ipc" "playerctl")
 
 not_installed=()
@@ -37,20 +35,20 @@ rm -rf picom/
 #copying dotfiles
 cd
 
-cp -r "$current_path/.config/i3/scripts" ~/.config/i3
-cp -r "$current_path/.config/i3/wallpapers" ~/.config/i3/
+cp -r "$HOME/.config/i3/scripts" ~/.config/i3
+cp -r "$HOME/.config/i3/wallpapers" ~/.config/i3/
 
 mkdir ~/.config/kitty/
-cp "$current_path/.config/kitty/kitty.conf" ~/.config
+cp "$HOME/.config/kitty/kitty.conf" ~/.config
 
 mkdir ~/.config/picom/
-cp "$current_path/.config/picom/picom.conf" ~/.config/picom/
+cp "$HOME/.config/picom/picom.conf" ~/.config/picom/
 
 mkdir ~/.config/polybar/
-cp -r "$current_path/.config/polybar/scripts" ~/.config/polybar/
-cp "$current_path/.config/polybar/config.ini" ~/.config/polybar/
-cp "$current_path/.config/polybar/launch.sh" ~/.config/polybar/
+cp -r "$HOME/.config/polybar/scripts" ~/.config/polybar/
+cp "$HOME/.config/polybar/config.ini" ~/.config/polybar/
+cp "$HOME/.config/polybar/launch.sh" ~/.config/polybar/
 
 mkdir ~/.config/rofi/
-cp -r "$current_path/.config/rofi/bin/" ~/.config/rofi/
-cp -r "$current_path/.config/rofi/config" ~/.config/rofi/
+cp -r "$HOME/.config/rofi/bin/" ~/.config/rofi/
+cp -r "$HOME/.config/rofi/config" ~/.config/rofi/
