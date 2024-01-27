652efb5f2e82eacc06d46e8b6d7ba1dc285ebdeb
idb <86549645+1db2@users.noreply.github.com>
2024-01-27 16:16:16 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -35,20 +35,20 @@ rm -rf picom/
 #copying dotfiles
 cd
 
-cp -r "$HOME/dotfiles/.config/i3/scripts" "$HOME.config/i3"
-cp -r "$HOME/dotfiles/.config/i3/wallpapers" "$HOME.config/i3/"
+cp -r "$HOME/dotfiles/.config/i3/scripts" "$HOME/.config/i3"
+cp -r "$HOME/dotfiles/.config/i3/wallpapers" "$HOME/.config/i3/"
 
 mkdir "$HOME/.config/kitty/"
-cp "$HOME/dotfiles/.config/kitty/kitty.conf" "$HOME.config"
+cp "$HOME/dotfiles/.config/kitty/kitty.conf" "$HOME/.config"
 
 mkdir "$HOME/.config/picom/"
-cp "$HOME/dotfiles/.config/picom/picom.conf" "$HOME.config/picom/"
+cp "$HOME/dotfiles/.config/picom/picom.conf" "$HOME/.config/picom/"
 
 mkdir "$HOME/.config/polybar/"
 cp -r "$HOME/dotfiles/.config/polybar/scripts" "$HOME/.config/polybar/"
-cp "$HOME/dotfiles/.config/polybar/config.ini" "$HOME.config/polybar/"
-cp "$HOME/dotfiles/.config/polybar/launch.sh" "$HOME.config/polybar/"
+cp "$HOME/dotfiles/.config/polybar/config.ini" "$HOME/.config/polybar/"
+cp "$HOME/dotfiles/.config/polybar/launch.sh" "$HOME/.config/polybar/"
 
 mkdir "$HOME/.config/rofi/
-cp -r "$HOME/dotfiles/.config/rofi/bin/" "$HOME.config/rofi/"
-cp -r "$HOME/dotfiles/.config/rofi/config" "$HOME.config/rofi/"
+cp -r "$HOME/dotfiles/.config/rofi/bin/" "$HOME/.config/rofi/"
+cp -r "$HOME/dotfiles/.config/rofi/config" "$HOME/.config/rofi/"
